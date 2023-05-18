2bebb36f9c59f355f0d3a54aec74e379d57af63b
Clinzho1996 <confidinho@yahoo.com>
2023-05-18 19:36:16 +0100

    new commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+import { fetchFromApi } from "../utils/fetchFromApi";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh", background: "#000" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 Dev-clinton's Media
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "red" }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
